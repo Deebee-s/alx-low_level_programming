ba1ec7d7f2bbe661e8b1809bda91aae87bf568a3
Debbie <debbybright320@gmail.com>
2023-02-16 14:37:04 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A programme that prints using put function
+ * Return: 0 (at_the_end)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+Return(0);
+}
