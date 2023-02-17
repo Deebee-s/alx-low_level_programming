82ef4cc35279b1a8b835f60897a0c2e172e7a2e8
Debbie <debbybright320@gmail.com>
2023-02-16 20:05:22 -0800

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
-/**
- * main - A c programme that prints a new line followed by a new line
- * Return: 1
+/** main - A c programme that prints a new line followed by a new line
+ * Return: Always 1
  */
+
+
 int main(void)
+
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
-return: (1);
+	write(2, "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
+return (1);
 }
