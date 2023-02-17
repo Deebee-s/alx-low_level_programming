aa7d9010a114cc992ba98f88e93e6d7cdc873375
Debbie <debbybright320@gmail.com>
2023-02-16 20:12:00 -0800

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
-/** main - A c programme that prints a new line followed by a new line
+/**
+ * main - A c programme that prints a new line followed by a new line
  * Return: Always 1
  */
 
@@ -7,6 +8,6 @@
 int main(void)
 
 {
-	write(2, "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
