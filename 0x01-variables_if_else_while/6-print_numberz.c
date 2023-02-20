c301ecf1d25df08b2ad903899d27eb19f015067e
Debbie <debbybright320@gmail.com>
2023-02-19 19:57:33 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - function
+ *
+ * Return: as stated
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
