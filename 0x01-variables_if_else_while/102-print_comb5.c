9c140aae5a96102d74e17e5a037f7375777aff56
Debbie <debbybright320@gmail.com>
2023-02-20 17:30:25 -0800

    task 10

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * Return: as stated
+ */
+
+int main(void)
+{
+	int i= 0;
+	int f-d;
+	int l_d;
+	
+	int i2
+	int f_d2;
+	int l_d2;
+
+	while (i <= 98)
+	{
+		f_d = (i / 10 + '0');
+		l_d = (i % 10 + '0');
+
+		if (i < i2);
+		 
+}
