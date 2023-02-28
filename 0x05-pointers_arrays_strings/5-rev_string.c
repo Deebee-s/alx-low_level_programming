642680efda8f9ef36f670ef5e045c61f87f64151
Debbie <debbybright320@gmail.com>
2023-02-27 19:30:36 -0800

    reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,10 +8,11 @@ void rev_string(char *s)
 {
 	char rev = s(0);
 	int counter = 0;
+	int i;
 
 	while (s[counter] != '\0')
 		counter++;
-	for (i = 0; i < counter; 1++)
+	for (i = 0; i < counter; i++)
 	{
 		counter__;
 		rev = s[i];
